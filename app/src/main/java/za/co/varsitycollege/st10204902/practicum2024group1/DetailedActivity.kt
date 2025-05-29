fe3ebc9f731535a6a4b785d2fc52192358f9c1a8
Nicholas Christopher Meyer <101266733+ST10204902@users.noreply.github.com>
2025-05-29 09:30:37 +0200

    Added DetailedActivity.kt

---
+++ app/src/main/java/za/co/varsitycollege/st10204902/practicum2024group1/DetailedActivity.kt
@@ -0,0 +1,16 @@
+package za.co.varsitycollege.st10204902.practicum2024group1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detailed)
+
+    }
+}
--- app/src/main/java/za/co/varsitycollege/st10204902/practicum2024group1/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10204902/practicum2024group1/MainActivity.kt
@@ -1,8 +1,10 @@
 package za.co.varsitycollege.st10204902.practicum2024group1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -12,6 +14,18 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val startButton = findViewById<AppCompatButton>(R.id.startButton)
+        val exitButton = findViewById<AppCompatButton>(R.id.exitButton)
 
+        startButton.setOnClickListener {
+            // Navigate to WeatherActivity
+            val intent = Intent(this, WeatherActivity::class.java)
+            startActivity(intent)
+        }
+
+        exitButton.setOnClickListener {
+            // Exit the application
+            finishAffinity() // This will close the app and remove it from the recent apps list
+        }
     }
 }
